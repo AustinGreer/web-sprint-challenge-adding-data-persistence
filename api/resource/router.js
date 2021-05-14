df95909ff343e02c54fd18a4b4666d4bd041c053
Austin Greer <agreer.ag12@gmail.com>
2021-05-14 19:45:31 -0400

    getting all resources is working

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,23 @@
-// build your `/api/resources` router here
+const express = require('express')
+const router = express.Router()
+const Resources = require('./model')
+
+
+router.get('/', async (req, res, next) => {
+    try {
+        const resources = await Resources.getAll()
+        res.json(resources)
+    } catch (err) {
+        next(err)
+    }
+})
+
+// error middleware
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
