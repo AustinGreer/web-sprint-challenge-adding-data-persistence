db6213c1ef484f51b615834a901e77987ad666e5
Austin Greer <agreer.ag12@gmail.com>
2021-05-14 18:52:41 -0400

    get all projects is now working

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,28 @@
-// build your `/api/projects` router here
+const express = require('express')
+const router = express.Router()
+const Projects = require('./model')
+
+// get - returns an array of project objs
+router.get('/', async (req, res, next) => {
+    try {
+        const allProjects = await Projects.getAll()
+        res.json(allProjects)
+    } catch (err) {
+        next(err)
+    }
+})
+
+// // post - returns an object of the newly created project
+// router.post('/', (req, res, next) => {
+    
+// })
+
+// error middleware
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // start your server here
 const server = require('./api/server')
 
-const PORT = process.env.PORT || 5000
+const PORT = 8000
 
 server.listen(PORT, () => {
     console.log(`Server Listening on Port ${PORT}`)
