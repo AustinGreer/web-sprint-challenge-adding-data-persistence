ad05676ec4bf6cee64f5e307b61fde693fb2c996
Austin Greer <agreer.ag12@gmail.com>
2021-05-14 15:24:49 -0400

    projects table set up complete

--- data/migrations/20210514191615_first-migration.js
+++ data/migrations/20210514191615_first-migration.js
@@ -1,6 +1,15 @@
 
-exports.up = function(knex) {
-    
+exports.up = async function(knex) {
+    await knex.schema
+    .createTable('projects', tbl => {
+        tbl.increments('project_id')
+        tbl.string('project_name', 30)
+            .notNullable()
+        tbl.string('project_description')
+        tbl.integer('project_completed')
+            .notNullable()
+            .defaultTo(0)
+    })
 };
 
 exports.down = function(knex) {
