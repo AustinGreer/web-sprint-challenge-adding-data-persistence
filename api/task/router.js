aac73e6ba45ea2435c760b994b432ae11ae90b46
Austin Greer <agreer.ag12@gmail.com>
2021-05-17 10:59:43 -0400

    scaffolding for getting all tasks completed. Just need to combine tables with projects and then endpoint will be working as expected

--- api/task/router.js
+++ api/task/router.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const Tasks = require('./model')
 
-router.get('/', (req, res, next) => {
-    res.json({message: 'api is working'})
+router.get('/', async (req, res, next) => {
+    try {
+        const allTasks = await Tasks.getAll()
+        res.json(allTasks)
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 module.exports = router
